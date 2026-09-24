fd05063a3b468d000b93d1530a86097d4e1880a8
booksbigchange-rgb <booksbigchange@gmail.com>
2026-09-24 22:16:08 +0300

    Add Big Change and upstream repository links

--- app/links.ts
+++ app/links.ts
@@ -1,7 +1,9 @@
 /** Destinations that appear in more than one panel. */
 
-/** The project's source, linked from the header byline and the About panel. */
-export const REPOSITORY = 'https://github.com/Yoosseph/pc-anatomy';
+/** This Big Change fork. */
+export const REPOSITORY = 'https://github.com/booksbigchange-rgb/pc-anatomy';
+/** Original MIT-licensed PC Anatomy project this learning edition is based on. */
+export const UPSTREAM_REPOSITORY = 'https://github.com/Yoosseph/pc-anatomy';
 /** The written companion to the explorer, served as a static page. */
 export const GUIDE = '/guide/index.html';
 /** The section of the guide that explains browser hardware acceleration. */
