6c675b22e7bbf22b8e112b250f78be50409b5063
booksbigchange-rgb <booksbigchange@gmail.com>
2026-09-24 22:47:58 +0300

    Make guide links Pages-safe

--- app/links.ts
+++ app/links.ts
@@ -5,6 +5,6 @@ export const REPOSITORY = 'https://github.com/booksbigchange-rgb/pc-anatomy';
 /** Original MIT-licensed PC Anatomy project this learning edition is based on. */
 export const UPSTREAM_REPOSITORY = 'https://github.com/Yoosseph/pc-anatomy';
 /** The written companion to the explorer, served as a static page. */
-export const GUIDE = '/guide/index.html';
+export const GUIDE = `${import.meta.env.BASE_URL}guide/index.html`;
 /** The section of the guide that explains browser hardware acceleration. */
 export const GUIDE_ACCELERATION = GUIDE + '#smoother-3d';
