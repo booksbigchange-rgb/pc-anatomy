c321ccb125920936718e10ba31e5cb37546243d2
booksbigchange-rgb <booksbigchange@gmail.com>
2026-09-25 16:25:18 +0300

    Disable analytics in school edition

--- app/main.tsx
+++ app/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
-import {Analytics} from '@vercel/analytics/react';
+import { createRoot } from 'react-dom/client';
 import Home from './page';
 import './globals.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><Home/><Analytics/></React.StrictMode>);
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+);
